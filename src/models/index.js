66cac3953eb81493746b765800fb831854dc9e70
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-18 15:50:30 -0700

    review

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,15 @@ const ClothesModel = require('./clothes');
 const Food = FoodModel(sequelize, Sequelize);
 const Clothes = ClothesModel(sequelize, Sequelize);
 
+
+// might help some code work!
+// const { Sequelize, DataTypes } = require('sequelize');
+
+// const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL;
+
+// const sequelizeDatabase = new Sequelize(DATABASE_URL);
+
+
 module.exports = {
   Food,
   Clothes,
--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const logger = require('./middleware/logger');
 const validator = require('./middleware/validator');
-const errorHandler404 = require('./error-handlers/404');
-const errorHandler500 = require('./error-handlers/500');
-
+const errorHandler404 = require('./error-handler/404');
+const errorHandler500 = require('./error-handler/500');
+// const cors = require('cors');
 const app = express();
 
 // Middleware
+
+// might be worth adding! not sure if required
+// app.use(cors());
+// app.use(express.json());
 app.use(logger);
 
 // Routes
+
+// app.get('/', (req, res, next) => {
+//   res.status(200).send('Server is alive!!!');
+// });
+
 app.get('/person', validator, (req, res) => {
   const name = req.query.name;
   if (name) {
@@ -18,6 +27,7 @@ app.get('/person', validator, (req, res) => {
     throw new Error('Name is missing');
   }
 });
+// very cool use of throwing an error!! didnt think of this!
 
 // Error Handlers
 app.use(errorHandler404);
